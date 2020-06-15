dff11bb14a60b713d7a537ae4c918247ad09081a
songcho99 <songcho99@gmail.com>
2020-06-15 21:24:41 +0900

    0615혜지수정완료

--- src/main/java/member/data/MemberController.java
+++ src/main/java/member/data/MemberController.java
@@ -4,9 +4,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import sms.data.ExampleSend;
+
 @RestController
 @CrossOrigin
 public class MemberController {
@@ -25,7 +31,11 @@ public class MemberController {
 	@Autowired
 	private MemberService service;
 	
-	@GetMapping("/login/loginck")
+	@Autowired
+	private JavaMailSender mailsender;
+	
+	//로그인
+	@PostMapping("/login/loginck")
 	public Map<String,String> loginck(@RequestParam String member_id, @RequestParam String member_password
 			,@RequestParam(required=false) String idsave,HttpSession session)
 	{
@@ -60,6 +70,7 @@ public class MemberController {
 		return idcheck;
 	}
 	
+	//회원 추가
 	@PostMapping("/member/insert")
 	public void insertMember(@RequestBody MemberDto memberdto)
 	{
@@ -68,6 +79,89 @@ public class MemberController {
 		service.insertMember(memberdto);
 	}
 	
+	//혜지 추가 부분
+	//아이디 찾기 위한 정보 확인
+	@PostMapping("/check/checkId")
+	public int checkId(@RequestParam String member_name, String member_email, String member_phone)
+	{
+		int cnt=0;
+		System.out.println("react>>checkId");
+		System.out.println(member_name);
+		System.out.println(member_email);
+		System.out.println(member_phone);
+		cnt=service.selectCountId(member_name, member_email, member_phone);
+		System.out.println(cnt);
+		return cnt;
+	}
 	
+	//비밀번호 재설정을 위한 정보 확인
+	@PostMapping("/check/checklogin")
+	public int checklogin(@RequestParam String member_name, String member_id, String member_phone)
+	{
+		int cnt=0;
+		System.out.println("react>>checklogin");
+		System.out.println(member_name);
+		System.out.println(member_id);
+		System.out.println(member_phone);
+		cnt=service.selectCount(member_name, member_id, member_phone);
+		System.out.println(cnt);
+		return cnt;
+	}
+	
+	//비밀번호 재설정
+	@PostMapping("/check/updatepassword")
+	public void updatePassword(@RequestParam String member_id, String member_password)
+	{
+		System.out.println("react>>updatepassword");
+		service.updatePassword(member_id, member_password);
+	}
+	
+	//인증번호 문자 발송
+	@PostMapping("/check/checknum")
+	public String checknum(@RequestParam String member_phone)
+	{
+		System.out.println("react>>checknum");
+		String randomsu="";
+		MemberDto cdto=service.admin();
+		String adminhp=cdto.getMember_phone();
+		
+		for(int i=0;i<6;i++)
+		{
+			int nansu=(int)(Math.random()*10);
+			randomsu+=Integer.toString(nansu);
+		}
+		System.out.println(member_phone);
+		
+		ExampleSend send=new ExampleSend();
+		send.sendSms(randomsu, member_phone, adminhp);
+		
+		return randomsu;
+	}
+	
+	//아이디 메일 발송
+	@PostMapping("/check/emailId")
+	public void emailId(@RequestParam String member_email)
+	{
+		System.out.println("react>>emailId");
+		System.out.println(member_email);
+		String member_id=service.selectId(member_email);
+		String email_content=member_id.substring(0, member_id.length()-2)+"**";
+		String content="회원님의 Id는 "+email_content+"입니다.";
+		
+		MimeMessage message=mailsender.createMimeMessage();
+		try {
+		
+			message.setSubject("IT Campus 아이디 확인");//메일제목
+			message.setText(content);//메일본문
+			message.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(member_email));//보낼 메일주소
+			mailsender.send(message);//메일전송
+
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("mail error="+e.getMessage());
+		}
+
+	}
 	
 }

c08cb5aece8366b57f6919058157bdec3077739d
sunghyun95 <jkok123@naver.com>
2020-06-30 19:16:27 +0900

    0630_수업자료CRUD

--- src/main/java/classdatafiles/data/ClassDataFilesDao.java
+++ src/main/java/classdatafiles/data/ClassDataFilesDao.java
@@ -42,6 +42,25 @@ public class ClassDataFilesDao extends SqlSessionDaoSupport implements ClassData
 				}
 	}
 
+	@Override
+	public void updateClassDataFiles(int classdatafiles_num) {
+		// TODO Auto-generated method stub
+		getSqlSession().update("updateOfClassDataFiles",classdatafiles_num);
+	}
+
+	@Override
+	public void deleteclassDataFiles(String classdatafiles_classdata_filename) {
+		// TODO Auto-generated method stub
+		getSqlSession().delete("deleteOfClassDataFiles",classdatafiles_classdata_filename);
+		
+	}
+
+	@Override
+	public List<String> selectClassDataFiles(int classdatafiles_classdata_num) {
+		// TODO Auto-generated method stub
+		return getSqlSession().selectList("selectOneOfClassDataFiles",classdatafiles_classdata_num);
+	}
+
 	
 
 }

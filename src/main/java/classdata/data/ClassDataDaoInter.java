b5603ca4b0a9843185bde4eae4adc3f989a921b0
sunghyun95 <jkok123@naver.com>
2020-07-01 12:00:15 +0900

    0701_수업자료(완성)

--- src/main/java/classdata/data/ClassDataDaoInter.java
+++ src/main/java/classdata/data/ClassDataDaoInter.java
@@ -7,7 +7,7 @@ public interface ClassDataDaoInter {
 	public void insertClassData(ClassDataDto dto);
 	public int selectMaxNum();
 	public ClassDataDto selectOne(int classdata_num);
-	public List<ClassDataDto> AllClassData();
+	public List<ClassDataDto> AllClassData(int process_num);
 	public void updateClassData(ClassDataDto dto);
 	public void deleteClassData(int classdata_num);
 }
